728382d90d0c19a403eb7b17f0e85b09fe60761a
Sumit2342003 <sumitpatil2342003@gmail.com>
2024-09-15 18:33:26 +0530

    SUMITP0001 - Added utility functions and custom sumit() method

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.java
@@ -0,0 +1,13 @@
+package com.example.javamavenjunithelloworld;
+
+public class Utility {
+    // Example utility function that adds two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    // Your custom function
+    public static void steve() {
+        System.out.println("I am Steve");
+    }
+}
